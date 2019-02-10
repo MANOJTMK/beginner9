f6bf380d08a2b4428c473cb6f20f04988d86075e
MANOJTMK <46492877+MANOJTMK@users.noreply.github.com>
2019-02-10 16:57:23 +0530

    Update 90.c

--- 90.c
+++ 90.c
@@ -4,7 +4,7 @@ int main(void)
 {
 	char str[50];
 	int i,n;
-	printf("\nenter the alphanumeric string:\n");
+	printf("enter the alphanumeric string:\n");
 	scanf("%s",str);
 	n=strlen(str);
 	for(i=0;i<n;i++)
