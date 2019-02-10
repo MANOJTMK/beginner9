fcd4d07c66e4f819dbe4f4e677d8413c43cea828
MANOJTMK <46492877+MANOJTMK@users.noreply.github.com>
2019-02-10 16:32:30 +0530

    Update 82.c

--- 82.c
+++ 82.c
@@ -3,7 +3,7 @@
 int main(void) 
 {
 	float l,b,a;
-	printf("enter the length and breadth values of the farm:");
+	printf("\nenter the length and breadth values of the farm:");
 	scanf("%f %f",&l,&b);
 	a=l*b;
 	printf("\n%0.5f",a);
